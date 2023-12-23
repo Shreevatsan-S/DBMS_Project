7573c3097ae71ee91c7238eb3777723d889978f4
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2023-12-23 14:56:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,138 +1,93 @@
-// const { MongoClient } = require('mongodb');
-
-// // Replace the connection string with your own MongoDB Atlas connection string
-// const uri = "mongodb+srv://hrenukunta66:hitesh66@cluster0.pfx1ved.mongodb.net/?retryWrites=true&w=majority";
-// // const uri = "mongodb+srv://hrenukunta66:hitesh66@cluster0.pfx1ved.mongodb.net";
-
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// // Connect to the MongoDB Atlas cluster
-// async function run() {
-//   try {
-//     await client.connect();
-//     console.log('Connected to the database');
-
-//     // Run your MongoDB shell command here
-//     const database = client.db('courierDB');
-//     // Replace the string with your own MongoDB shell command
-//     const commandResult = await database.command({ "db.collection.find()": 1 });
-//     console.log('Command executed:', commandResult);
-
-//   } finally {
-//     // Close the connection to the MongoDB Atlas cluster
-//     await client.close();
-//   }
-// }
-
-// run().catch(console.dir);
-
+const express = require('express');
+const app = express();
 const { MongoClient } = require('mongodb');
+const { Statement } = require('sqlite3');
 
-// Replace the connection string with your own MongoDB Atlas connection string
 const uri = "mongodb+srv://hrenukunta66:hitesh66@cluster0.pfx1ved.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {});
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+let input=`db.licenses.find({id:101586})`
+let collections=["crime_scene_reports" , "persons"  , "incomes" , "interviews" , "licenses" , "get_fit_now_check_ins" , "get_fit_now_members" , "facebook_event_checkins"]
 
-// Connect to the MongoDB Atlas cluster
+let first , second , third , query
+
+function queryProcessor(str){
+    first=str.slice(0 , 3)
+    if(first != "db."){
+      console.error("wrong query 1")
+    }
+    else{
+        let x=str.indexOf("." , 3)
+        second=str.slice(3,x)
+        if(collections.includes(second)){
+            if(str[x]=="." ){
+                let y= str.indexOf("(" , x+1)
+                third=str.slice(x+1 , y)
+                let l=str.length
+                if(str[l-1]==")"){
+                    query=str.slice(y+1 , l-1)
+                    console.log(second , third , query)
+                    query=query.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ')
+                }
+                else{
+                    console.error("wrong error 4")
+                }
+            }
+            else{
+                console.error("wrong query 3")
+            }
+        }
+        else{
+            console.error("wrong query 2")
+        }
+    }
+  }
+
 async function run() {
+    queryProcessor(input)
   try {
     await client.connect();
     console.log('Connected to the database');
 
-    const database = client.db('courierDB');
-    const collection = database.collection('places'); // Specify your collection name here
+    const database = client.db('crimeDB');
+    const collection = database.collection(second); 
+    let documents
 
-    // Replace the string with your own MongoDB shell command
-    // const mongoShellCommand = 'db.collection.find()'; // Replace with your own MongoDB shell command
-
-    // Parsing the collection name from the shell command
-    // const collectionName = mongoShellCommand.split('.')[1];
-    // const collection = database.collection(collectionName);
-
-    // Execute the MongoDB shell command on the specified collection
-    // Example: find all documents in the collection
-    const query = {}; // you can replace this with your own query
-    const documents = await collection.find(query).toArray();
-    console.log('Documents in the collection:', documents);
-
-  } finally {
-    // Close the connection to the MongoDB Atlas cluster
+    if(third=="find"){
+        documents = await collection.find(JSON.parse(query)).toArray();
+        console.log('Documents in the collection:', documents);
+    }
+    else if(third=="findOne"){
+        documents = await collection.findOne(JSON.parse(query)).toArray();
+        console.log('Documents in the collection:', documents);
+    }
+    else if(third=="aggregate"){
+        documents = await collection.aggregate(JSON.parse(query)).toArray();
+        console.log('Documents in the collection:', documents);
+    }
+    else{
+        console.error("cannot be executed")
+    }
+  }
+  finally {
     await client.close();
   }
 }
 
-run().catch(console.dir);
-
-
-// const { MongoClient } = require('mongodb');
-
-// // Connection URI
-// const uri = "mongodb+srv://hrenukunta66:hitesh66@cluster0.pfx1ved.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a new MongoClient
-// const client = new MongoClient(uri);
-
-// async function run() {
-//   try {
-//     // Connect the client to the server
-//     await client.connect();
-
-//     // Get a reference to the database
-//     const database = client.db('courierDB');
-
-//     // Define the command string
-//     const commandString = 'db.orders.find()';
-
-//     // Execute the command using eval
-//     const result = await database.command({ eval: commandString });
-
-
-//     // Show the result
-//     console.log(result);
-//   } finally {
-//     // Close the connection
-//     await client.close();
-//   }
-// }
-
 // run().catch(console.dir);
 
 
-// const { MongoClient } = require('mongodb-runner');
+app.get('/run', async (req, res) => {
+    try {
+      await run();
+      res.send('Operation completed successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('An error occurred');
+    }
+  });
+  
+  // exports.app = functions.https.onRequest(app);
 
-// // Connection URI
-// const uri = "mongodb+srv://hrenukunta66:hitesh66@cluster0.pfx1ved.mongodb.net/?retryWrites=true&w=majority";
 
-// // Create a new MongoClient
-// const client = new MongoClient(uri);
-
-// async function run() {
-//   try {
-//     // Connect the client to the server
-//     await client.connect();
-
-//     // Get a reference to the database
-//     const database = client.db('courierDB');
-
-//     // Define the command string
-//     const commandString = 'db.orders.find()';
-
-//     // Execute the command using eval
-//     const result = await database.execute(commandString);
-
-//     // Show the result
-//     console.log(result);
-//   } finally {
-//     // Close the connection
-//     await client.close();
-//   }
-// }
-
-// run().catch(console.dir);
-
+  app.listen(3000)
