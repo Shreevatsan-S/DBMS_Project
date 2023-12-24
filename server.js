06d471f4aa6445e1230d0293abb92093b16b0ecb
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2023-12-24 10:54:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
 const { Statement } = require('sqlite3');
+const cors = require('cors');
+
 
 const uri = "mongodb+srv://hrenukunta66:hitesh66@cluster0.pfx1ved.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {});
 
+app.use(cors());
+
 let input=`db.licenses.find({id:101586})`
 let collections=["crime_scene_reports" , "persons"  , "incomes" , "interviews" , "licenses" , "get_fit_now_check_ins" , "get_fit_now_members" , "facebook_event_checkins"]
 
-let first , second , third , query
+let first , second , third , query, errMsg
 
 function queryProcessor(str){
     first=str.slice(0 , 3)
     if(first != "db."){
       console.error("wrong query 1")
+      errMsg = "wrong query 1"
     }
     else{
         let x=str.indexOf("." , 3)
@@ -31,14 +36,17 @@ function queryProcessor(str){
                 }
                 else{
                     console.error("wrong error 4")
+                    errMsg = "wrong query 4"
                 }
             }
             else{
                 console.error("wrong query 3")
+                errMsg = "wrong query 3"
             }
         }
         else{
             console.error("wrong query 2")
+            errMsg = "wrong query 2"
         }
     }
   }
@@ -55,18 +63,22 @@ async function run() {
 
     if(third=="find"){
         documents = await collection.find(JSON.parse(query)).toArray();
-        console.log('Documents in the collection:', documents);
+        // console.log('Documents in the collection:', documents);
+        return documents
     }
     else if(third=="findOne"){
         documents = await collection.findOne(JSON.parse(query)).toArray();
-        console.log('Documents in the collection:', documents);
+        // console.log('Documents in the collection:', documents);
+        return documents
     }
     else if(third=="aggregate"){
         documents = await collection.aggregate(JSON.parse(query)).toArray();
-        console.log('Documents in the collection:', documents);
+        // console.log('Documents in the collection:', documents);
+        return documents
     }
     else{
         console.error("cannot be executed")
+        return errMsg
     }
   }
   finally {
@@ -80,8 +92,9 @@ console.log("serverrr")
 app.get('/', async (req, res) => {
     console.log("requested")
     try {
-      await run();
-      res.send('Operation completed successfully');
+      const resp = await run();
+      console.log(resp)
+      res.send(resp);
     } catch (error) {
       console.error(error);
       res.status(500).send('An error occurred');
